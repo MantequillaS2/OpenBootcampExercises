dffd0a285dea9eb07b321bfcb0d24d97fff515dd
MantequillaS2 <109777573+MantequillaS2@users.noreply.github.com>
2022-09-08 00:27:59 -0500

    Add files via upload

---
+++ Homework_2/src/homework/AllMethods.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class AllMethods {
+    public static void main(String[] args) {
+        System.out.println("The different kind of data are: ");
+        System.out.println("int");
+        System.out.println("byte");
+        System.out.println("long");
+        System.out.println("short");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("boolean");
+        System.out.println("String");
+        System.out.println("char");
+
+    }
+}
